b329412255235ea7b33000e3ecea79c116293e36
Mishael <magsanoc@live.com>
2021-10-12 23:47:47 -0400

    Fixed storage by using local storage and messages

--- background.js
+++ background.js
@@ -11,8 +11,120 @@ chrome.runtime.onInstalled.addListener(() => {
   var startFlag = false;
 
   // Sets background state of values
-  chrome.storage.sync.set({ countdownTimer });
-  chrome.storage.sync.set({ intervalTimer });
-  chrome.storage.sync.set({ distanceTime });
-  chrome.storage.sync.set({ startFlag });
+  chrome.storage.local.set({ countdownTimer });
+  chrome.storage.local.set({ intervalTimer });
+  chrome.storage.local.set({ distanceTime });
+  chrome.storage.local.set({ startFlag });
 });
+
+// Listens for connection
+chrome.runtime.onConnect.addListener(function (port) {
+  console.assert(port.name === 'Pomodoro');
+  port.onMessage.addListener(function (msg) {
+    // Starts the timer as the start button was pressed
+    if (msg.cmd === 'start') {
+      console.log('STARTING SOON');
+      if (++timesStarted > 1) {
+        console.log('Too many times started');
+        timesStarted = 1;
+      } else {
+        if (timesPaused == 1) {
+          --timesPaused;
+        }
+        console.log('Time started = ' + timesStarted);
+        chrome.storage.local.set({ startFlag: true }, function () {
+          countdownDriver(port);
+        });
+      }
+      // Pauses the timer as the pause button was pressed
+    } else if (msg.cmd === 'pause') {
+      console.log('PAUSED!');
+      if (++timesPaused > 1) {
+        console.log('TOO MANY TIMES PAUSED');
+        timesPaused = 1;
+      } else {
+        if (timesStarted == 1) {
+          --timesStarted;
+        }
+        console.log('Time paused = ' + timesPaused);
+        chrome.storage.local.set({ startFlag: false }, function () {
+          chrome.storage.local.set(
+            { distanceTime: distanceTimeGlobal },
+            function () {
+              console.log('Distance time is set to ' + distanceTimeGlobal);
+            }
+          );
+          clearInterval(intervalTimer);
+        });
+      }
+    }
+  });
+});
+
+/*
+Start of application logic for pomodoro
+*/
+// Global variables found for popup
+var distanceTimeGlobal;
+var countdownTimer;
+var hours;
+var minutes;
+var seconds;
+var intervalTimer;
+var timesPaused = 0,
+  timesStarted = 0;
+
+// Output the result in an element with id="countdownTimer"
+function printCountdownTimer() {
+  document.getElementById('countdownTimer').innerHTML =
+    hours + 'h ' + minutes + 'm ' + seconds + 's ';
+}
+
+// Countdown function driver
+function countdownDriver(port) {
+  chrome.storage.local.get('distanceTime', (result) => {
+    distanceTimeGlobal = result.distanceTime;
+    if (distanceTimeGlobal >= 0) {
+      countdownTimer = new Date().getTime() + distanceTimeGlobal;
+      console.log('Countdown timer' + countdownTimer);
+    }
+    countdownStart(countdownTimer, port);
+  });
+}
+
+// Starts the countdown function
+function countdownStart(countdown, port) {
+  intervalTimer = setInterval(() => {
+    // Find distanceTime between now and countdown date
+    var timeNow = new Date().getTime();
+
+    distanceTimeGlobal = countdown - timeNow;
+
+    console.log('the time is ' + distanceTimeGlobal);
+
+    // Time calculations for days, hours, minutes and seconds
+    updateTimer(distanceTimeGlobal);
+
+    // Update distance time in local storage
+    chrome.storage.local.set({ distanceTime: distanceTimeGlobal }, function () {
+      console.log('Distance time is set to ' + distanceTimeGlobal);
+    });
+
+    // Output the result in an element with id="countdownTimer"
+    port.postMessage({ response: 'update' });
+
+    // If the count down is over, write some text
+    if (distanceTimeGlobal < 0) {
+      clearInterval(intervalTimer);
+      port.postMessage({ response: 'expired' });
+    }
+  }, 500);
+}
+
+// Updates hours, minutes, seconds
+function updateTimer(distanceTime) {
+  // Time calculations for days, hours, minutes and seconds
+  hours = Math.floor((distanceTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  minutes = Math.floor((distanceTime % (1000 * 60 * 60)) / (1000 * 60));
+  seconds = Math.floor((distanceTime % (1000 * 60)) / 1000);
+}
--- popup.js
+++ popup.js
@@ -8,94 +8,13 @@ Block websites
 Add notes? - Note taking for each session?
 */
 
-// Global variables found for popup
-var distanceTimeGlobal;
-var countdownTimer;
-var hours;
-var minutes;
-var seconds;
-var intervalTimer;
-
-// Output the result in an element with id="countdownTimer"
-function printCountdownTimer() {
-  document.getElementById('countdownTimer').innerHTML =
-    hours + 'h ' + minutes + 'm ' + seconds + 's ';
-}
-
-// Placeholder when popup is clicked
-function printCountdownTimerPlaceholder() {
-  chrome.storage.sync.get('startFlag', function (result) {
-    if (result.startFlag == true) {
-      countdownDriver();
-    } else {
-      chrome.storage.sync.get('distanceTime', function (result) {
-        distanceTimeGlobal = result.distanceTime;
-        updateTimer(distanceTimeGlobal);
-        document.getElementById('countdownTimer').innerHTML =
-          hours + 'h ' + minutes + 'm ' + seconds + 's ';
-      });
-    }
-  });
-}
-printCountdownTimerPlaceholder();
-
-// Countdown function driver
-function countdownDriver() {
-  chrome.storage.sync.get('distanceTime', (result) => {
-    distanceTimeGlobal = result.distanceTime;
-    console.log('The saved distance time outside is ' + distanceTimeGlobal);
-    if (distanceTimeGlobal >= 0) {
-      countdownTimer = new Date().getTime() + distanceTimeGlobal;
-      console.log('Countdown timer' + countdownTimer);
-    }
-    countdownStart(countdownTimer);
-  });
-}
-
-// Starts the countdown function
-function countdownStart(countdown) {
-  intervalTimer = setInterval(() => {
-    // Find distanceTime between now and countdown date
-    var timeNow = new Date().getTime();
-
-    distanceTimeGlobal = countdown - timeNow;
-
-    console.log('the time is ' + distanceTimeGlobal);
-
-    // Time calculations for days, hours, minutes and seconds
-    updateTimer(distanceTimeGlobal);
-
-    // Output the result in an element with id="countdownTimer"
-    printCountdownTimer();
-
-    // Update distance time in local storage
-    chrome.storage.sync.set({ distanceTime: distanceTimeGlobal }, function () {
-      console.log('Distance time is set to ' + distanceTimeGlobal);
-    });
-
-    // If the count down is over, write some text
-    if (distanceTimeGlobal < 0) {
-      clearInterval(intervalTimer);
-      document.getElementById('countdownTimer').innerHTML = 'EXPIRED';
-    }
-  }, 500);
-}
-
-// Updates hours, minutes, seconds
-function updateTimer(distanceTime) {
-  // Time calculations for days, hours, minutes and seconds
-  hours = Math.floor((distanceTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  minutes = Math.floor((distanceTime % (1000 * 60 * 60)) / (1000 * 60));
-  seconds = Math.floor((distanceTime % (1000 * 60)) / 1000);
-}
-
 // Adds 1 minute to timer
 addTime.addEventListener('click', () => {
   distanceTimeGlobal += 60000;
   updateTimer(distanceTimeGlobal);
   document.getElementById('countdownTimer').innerHTML =
     hours + 'h ' + minutes + 'm ' + seconds + 's ';
-  chrome.storage.sync.set({ distanceTime: distanceTimeGlobal }, function () {
+  chrome.storage.local.set({ distanceTime: distanceTimeGlobal }, function () {
     console.log('Distance time is set to ' + distanceTimeGlobal);
   });
 });
@@ -106,7 +25,7 @@ subtractTime.addEventListener('click', () => {
     updateTimer(distanceTimeGlobal);
     document.getElementById('countdownTimer').innerHTML =
       hours + 'h ' + minutes + 'm ' + seconds + 's ';
-    chrome.storage.sync.set({ distanceTime: distanceTimeGlobal }, function () {
+    chrome.storage.local.set({ distanceTime: distanceTimeGlobal }, function () {
       console.log('Distance time is set to ' + distanceTimeGlobal);
     });
   }
@@ -114,18 +33,95 @@ subtractTime.addEventListener('click', () => {
 
 // Starts the countdown timer
 buttonStart.addEventListener('click', () => {
-  chrome.storage.sync.set({ startFlag: true }, function () {
-    countdownDriver();
-  });
+  playAudio();
+  sendMessageStart();
 });
 
 // Pauses the countdown timer
 buttonPause.addEventListener('click', () => {
-  chrome.storage.sync.set({ startFlag: false }, function () {
-    chrome.storage.sync.set({ distanceTime: distanceTimeGlobal }, function () {
-      console.log('Distance time is set to ' + distanceTimeGlobal);
-    });
-  });
-
-  clearInterval(intervalTimer);
+  sendMessagePause();
 });
+
+// Send messages to background
+function sendMessageStart() {
+  port.postMessage({ cmd: 'start' });
+}
+function sendMessagePause() {
+  port.postMessage({ cmd: 'pause' });
+}
+
+// Create connection
+var port = chrome.runtime.connect({ name: 'Pomodoro' });
+
+port.onMessage.addListener(function (msg) {
+  if (msg.response === 'update') {
+    console.log('RESPONSED RECEIVED');
+    chrome.storage.local.get('distanceTime', (result) => {
+      console.log(
+        'The saved distance time in extension is ' + result.distanceTime
+      );
+      updateTimer(result.distanceTime);
+      updateHTMLTime();
+    });
+  } else if (msg.response === 'expired') {
+    playAudio();
+    document.getElementById('countdownTimer').innerHTML = 'expired';
+  }
+});
+
+// Global variables for timer update
+var hours;
+var minutes;
+var seconds;
+
+// Update Timer HTML
+function updateHTMLTime() {
+  document.getElementById('countdownTimer').innerHTML =
+    hours + 'h ' + minutes + 'm ' + seconds + 's ';
+}
+
+// Placeholder when popup is clicked
+function printCountdownTimerPlaceholder() {
+  chrome.storage.local.get('startFlag', function (result) {
+    if (result.startFlag == true) {
+      chrome.storage.local.get('distanceTime', function (result) {
+        distanceTimeGlobal = result.distanceTime;
+        updateTimer(distanceTimeGlobal);
+        updateHTMLTime();
+        sendMessagePause();
+        sendMessageStart();
+      });
+    } else {
+      chrome.storage.local.get('distanceTime', function (result) {
+        distanceTimeGlobal = result.distanceTime;
+        updateTimer(distanceTimeGlobal);
+        updateHTMLTime();
+      });
+    }
+  });
+}
+printCountdownTimerPlaceholder();
+
+// Updates hours, minutes, seconds
+function updateTimer(distanceTime) {
+  // Time calculations for days, hours, minutes and seconds
+  hours = Math.floor((distanceTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  minutes = Math.floor((distanceTime % (1000 * 60 * 60)) / (1000 * 60));
+  seconds = Math.floor((distanceTime % (1000 * 60)) / 1000);
+}
+
+// Play sound
+function chooseAudio() {
+  var soundURL = [
+    'https://actions.google.com/sounds/v1/alarms/alarm_clock.ogg',
+    'https://actions.google.com/sounds/v1/alarms/bugle_tune.ogg',
+    'https://actions.google.com/sounds/v1/alarms/radiation_meter.ogg',
+    'https://actions.google.com/sounds/v1/cartoon/concussive_guitar_hit.ogg',
+    'https://actions.google.com/sounds/v1/household/window_close_series.ogg',
+  ];
+}
+function playAudio() {
+  var audioElement = new Audio('https://onlineclock.net/sounds/?sound=Default');
+
+  audioElement.play();
+}
